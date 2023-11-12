e2374ae7d77afbe5a4176028f70c2ffe15d3bf05
mariemk <101462688+MariemKhlif@users.noreply.github.com>
2023-11-12 12:43:43 +0100

    Add files via upload

---
+++ Tp3DevAppRep/src/ac3_1/Serveur.java
@@ -0,0 +1,26 @@
+package ac3_1;
+import java.io.*;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Serveur extends Thread {
+	public static void main(String[] args) {
+		int ord = 0 ;
+
+		// TODO Auto-generated method stub
+		try {
+		ServerSocket socketServeur = new ServerSocket(1234);
+		System.out.println(" Le serveur attend la connexion d'un client ");
+	     while(true){
+		 Socket socket = socketServeur.accept();
+		 System.out.println(" le client est connecte ");
+		 ClientProcess client = new ClientProcess(socket, ++ord);
+		 client.start();
+ 	     socket.close(); }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}}}
+
+	
